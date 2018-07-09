0d642ca280044f509a0b57b1e917fe6d19446851
xiaokang pu <pxk@seiue.com>
2018-07-09 23:34:40 +0800

    new parse

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
  */
 function parse(lines) {
     return lines.map(line => {
-        console.log(line)
-        return JSON.parse(`[${line}]`)
+        const items = line.replace(/""/g, '"').split(',')
+        return items.map(item => {
+            if (item.charAt(0) === '"') {
+                return item.slice(1, -1)
+            } else if (isFinite(item)) {
+                return Number(item)
+            } else {
+                return item
+            }
+        })
     })
 }
 
